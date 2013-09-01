4ba51bba02bb9e69314cc7b4f44175a9a5502c11
Robert Day <rkd@rkd.me.uk>
2013-09-01 17:38:48 +0100

    Combine emacs files into a single emacs.el

--- .emacs.el
+++ .emacs.el
@@ -1,10 +1,26 @@
+(require 'package)
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/"))
+(package-initialize)
+
+(defvar my-packages '(starter-kit
+                      starter-kit-lisp
+                      starter-kit-bindings
+                      starter-kit-eshell
+                      clojure-mode
+                      clojure-test-mode
+                      nrepl))
+
+(dolist (p my-packages)
+  (when (not (package-installed-p p))
+    (package-install p)))
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(browse-url-browser-function (quote browse-url-firefox))
- '(browse-url-firefox-new-window-is-tab t)
+ '(browse-url-browser-function (quote browse-url-chromium))
  '(electric-indent-mode t)
  '(electric-pair-mode t)
  '(global-hl-line-mode t)
