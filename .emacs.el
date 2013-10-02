bf7fae1e1fedba4dfc47f61c0711b5e80dd4f2f1
Rob Day <rkd@rkd.me.uk>
2013-10-02 23:45:22 +0100

    Nicer theme and fonts

--- .emacs.el
+++ .emacs.el
@@ -11,7 +11,8 @@
                       clojure-test-mode
                       nrepl
                       yasnippet
-                      yasnippet-bundle))
+                      yasnippet-bundle
+                      color-theme-sanityinc-tomorrow))
 
 (dolist (p my-packages)
   (when (not (package-installed-p p))
@@ -23,6 +24,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(browse-url-browser-function (quote browse-url-chromium))
+ '(custom-safe-themes (quote ("628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "82d2cac368ccdec2fcc7573f24c3f79654b78bf133096f9b40c20d97ec1d8016" "1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" "5ee12d8250b0952deefc88814cf0672327d7ee70b16344372db9460e9a0e3ffc" default)))
  '(electric-indent-mode t)
  '(electric-pair-mode t)
  '(global-hl-line-mode t)
@@ -47,8 +49,8 @@
  '(whitespace-line ((t (:background "black" :foreground "red"))))
  '(whitespace-tab ((t (:background "DeepPink1" :foreground "lightgray")))))
 
-(load-theme 'tango)
-(set-default-font " -unknown-DejaVu Sans Mono-normal-normal-normal-*-12-*-*-*-m-0-iso10646-1")
+(load-theme 'sanityinc-tomorrow-bright)
+(set-default-font "Source Code Pro:pixelsize=11:foundry=adobe:weight=light:slant=normal:width=normal:spacing=100:scalable=true")
 (yas/global-mode 1)
 (global-set-key (kbd "<f10>") 'yas/insert-snippet)
 (global-set-key (kbd "<f5>") 'compile)
@@ -68,3 +70,4 @@
 (add-hook 'lisp-mode-hook       'hs-minor-mode)
 (add-hook 'perl-mode-hook       'hs-minor-mode)
 (add-hook 'sh-mode-hook         'hs-minor-mode)
+
